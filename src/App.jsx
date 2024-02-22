566bcd58905be36c4c6c0676ace190ccd3aab7d9
Inesh-Reddy <ineshreddy007@gmail.com>
2024-02-22 13:27:24 +0530

    lazy load update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import { useState,React, lazy, Suspense } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-const Dashboard = lazy(()=>import('./components/Dashboard'))
-const Landing = lazy(()=>import('./components/Landing'))
-const Title = lazy(()=>import('./components/Title'))
-
+import { useState, React, lazy, Suspense } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
+const Title = lazy(() => import("./components/Title"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <BrowserRouter>
         <Title />
         <Routes>
-          <Route path='/Dashboard' element={<Suspense fallback={"loading..."} ><Dashboard /></Suspense> } ></Route>
-          <Route path="/" element={<Suspense fallback={"loading..."} >< Landing /></Suspense> } ></Route>
+          <Route
+            path="/Dashboard"
+            element={
+              <Suspense fallback={"loading..."}>
+                <Dashboard />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"loading..."}>
+                <Landing />
+              </Suspense>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
-
-
-
-
-export default App
+export default App;
